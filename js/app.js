8af64efb32f8d88e6ba4b6240329672d06577808
Cynthia Chaves <cynthia_chaves@aol.com>
2024-09-06 01:07:27 -0400

    Finished gameplay logic and added styling.

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const wordList = [
     },
     {
         word: "canine",
-        hint: "An animal considered to be mans's best friend."
+        hint: "An animal considered to be man's best friend."
     },
     {
         word: "vehicle",
@@ -22,15 +22,12 @@ const wordList = [
     },
 ];
 
-
 /*---------- Variables (state) ---------*/
 let currentWord;
 let correctLtrGuesses;
 let incorrectGuessCount;
-
-
+let button;
 const maxGuesses = 6;
-
 
 /*----- Cached Element References  -----*/
 const wordDisplay = document.querySelector('.word-display');
@@ -39,10 +36,18 @@ const incorrectGuesses = document.querySelector('.incorrect-guesses b');
 const keyboard = document.querySelector('.keyboard');
 const spacemanImg = document.querySelector('.welcome-display img');
 const resultsDisplay = document.querySelector('.results-display');
-const resetBtn = document.querySelector('.reset-button'); 
+const resetBtn = document.querySelector('.reset-button');
 
 /*-------------- Functions -------------*/
+const wordSelector = (() => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    hintDisplay.textContent = hint;
+    //init();
+});
+
 const init = (() => {
+    wordSelector();
     correctLtrGuesses = [];
     incorrectGuessCount = 0;
     spacemanImg.src = 'images/hangman-0.svg';
@@ -50,20 +55,50 @@ const init = (() => {
     wordDisplay.innerHTML = currentWord.split('').map(() => `<li class='letter'></li>`).join('');
     keyboard.querySelectorAll('button').forEach((button) => button.disabled = false);
     resultsDisplay.classList.remove('show');
-    //render();
 });
 
-const wordSelector = (() => {
-    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
-    currentWord = word;
-    hintDisplay.textContent = hint;
-    //console.log(currentWord);
-    init();
+const endGameDisplay = ((isWin) => {
+    const resultsText = isWin ? `You found the correct word: ` : `The correct word was: `;
+    resultsDisplay.querySelector('img').src = `images/${isWin ? 'victory' : 'lost'}.gif`;
+    resultsDisplay.querySelector('h4').innerText = isWin ? 'Congratulations! You Won!' : 'Please try again';
+    resultsDisplay.querySelector('p').innerHTML = `${resultsText} <b>${currentWord}</b>`;
+    resultsDisplay.classList.add('show');
 });
 
-wordSelector();
+for (let i = 97; i <= 122; i++) {
+    button = document.createElement('button');
+    button.innerText = String.fromCharCode(i);
+    keyboard.appendChild(button);
+    button.addEventListener('click', (event) => gamePlay(event.target, String.fromCharCode(i)));
+};
 
+const gamePlay = ((button, selectedLetter) => {
+    if (currentWord.includes(selectedLetter)) {
+        [...currentWord].forEach((letter, index) => {
+        if (letter === selectedLetter) {
+          correctLtrGuesses.push(letter);
+          wordDisplay.querySelectorAll('li')[index].innerText = letter;
+          wordDisplay.querySelectorAll('li')[index].classList.add('guesses');
+        }
+      });
+    }
+    else {
+      incorrectGuessCount++;
+      spacemanImg.src = `images/hangman-${incorrectGuessCount}.svg`;
+    }
+    button.disabled = true;
+    incorrectGuesses.innerText = `${incorrectGuessCount} / ${maxGuesses}`;
+    if (correctLtrGuesses.length === currentWord.length) {
+      return endGameDisplay(true);
+    }
+    else if (incorrectGuessCount === maxGuesses) {
+        return endGameDisplay(false);
+    };
+});
 
+init();
 
 /*----------- Event Listeners ----------*/
+
+resetBtn.addEventListener('click', (init));
 
